4799225255be45120a6e61b5eb4a8c3909a90679
Scott Barron <develop.toolbox@gmail.com>
2013-11-22 12:18:17 -0800

    Now includes arpy.GetParameterAsText inputs.

--- NetCDF-To-Raster/NetCDF_Converter_Tool.py
+++ NetCDF-To-Raster/NetCDF_Converter_Tool.py
@@ -9,55 +9,29 @@ import os
 import arcpy
 from arcpy import env
 #The folder_path is the name and location of the folder containing the NetCDF files, and where the rasters will be created.
-folder= raw_input("Please input the name and location of the folder containing the NetCDFs: ")
-folder_path= r"%s" %(folder)
-arcpy.env.workspace= folder_path
+Input_folder= arcpy.GetParameterAsText(0)
+arcpy.env.workspace= Input_folder
 
 #This is the variable to display from the NetCDF file. To assign the default value, leave as NetCDF_Var= "".
-Variable= raw_input("If you would like to use the default value for 'Variable', type 'Yes'. If not, type 'No'" )
-Var= "%s" %Variable
-if Var == "Yes":
-    NetCDF_Var= ""
-elif Var == "No":
-    Var2= raw_input("Please enter the 'Variable' value: ")
-    NetCDF_Var= "%s" %Var2
-else:
-    print "You entered an invalid command. Please restart the code and enter either Yes or No when prompted."
+Variable= arcpy.GetParameterAsText(1)
                     
 #This is the X dimension from the NetCDF file. To assign the default value, leave as NetCDF_X= "".
-X_Value= raw_input("If you would like to use the default value for 'X dimension', type 'Yes'. If not, type 'No'")
-Value= "%s" %X_Value
-if Value == "Yes":
-    NetCDF_X= ""
-elif Value == "No":
-    X_Value2= raw_input("Please enter the 'X dimension' value: ")
-    NetCDF_X= "%s" %X_Value2
-else:
-    print "You entered an invalid command. Please restart the code and enter either Yes or No when prompted."
+X_Value= arcpy.GetParameterAsText(2)
     
 #This is the Y dimension from the NetCDF file. To assign the default value, leave as NetCDF_Y= "".
-Y_Value= raw_input("If you would like to use the default value for 'Y dimension', type 'Yes'. If not, type 'No'")
-Value= "%s" %Y_Value
-if Value == "Yes":
-    NetCDF_Y= ""
-elif Value == "No":
-    Y_Value2= raw_input("Please enter the 'Y dimension' value: ")
-    NetCDF_Y= "%s" %Y_Value2
-else:
-    print "You entered an invalid command. Please restart the code and enter either Yes or No when prompted."
+Y_Value= arcpy.GetParameterAsText(3)
 
 #This is the name and location where the rasters will be generated.
-Output_Location= raw_input('Where would you like the generated rasters to be placed?' )
-Output= r"%s" %Output_Location
+Output_Location= arcpy.GetParameterAsText(4)
 
 #This for loop will search through the folder designated by folder_path and if a file ends in ".nc", then this script will convert it to a raster.
 for NetCDFs in os.listdir(folder_path):
     if NetCDFs[-3:]== ".nc":
         NetCDF_Name= NetCDFs[5:13]
         print NetCDF_Name
-        print Output + "/" + NetCDF_Name
-        arcpy.MakeNetCDFRasterLayer_md(NetCDFs,NetCDF_Var,NetCDF_X,NetCDF_Y,NetCDF_Name)
+        print Output_Location + "/" + NetCDF_Name
+        arcpy.MakeNetCDFRasterLayer_md(NetCDFs,Variable,X_Value,Y_Value,NetCDF_Name)
         Raster_Name= "x" + str(NetCDF_Name)
-        arcpy.CopyRaster_management(NetCDF_Name,Output + "/" + Raster_Name)
+        arcpy.CopyRaster_management(NetCDF_Name,Output_Location + "/" + Raster_Name)
 
-print "The rasters have been successfully generated."
+arcpy.AddMessag("The rasters have been successfully generated.")
