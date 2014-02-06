032459a4b05560a4bc6d9443e3c1bfd6e0847e79
Scott Barron <develop.toolbox@gmail.com>
2014-02-06 12:13:48 -0800

    Added EBM and Clip ArcGIS tools.

--- Basic_Batch_Tools/Batch_Clip.py
+++ Basic_Batch_Tools/Batch_Clip.py
@@ -5,18 +5,18 @@ if arcpy.CheckExtension("Spatial")== "Available":
     from arcpy.sa import *
 else:
     arcpy.AddError("You do not have the Spatial Analyst Extension, and therefore cannot use this tool.")
-    
+
 #Input folder.
 folder_path= arcpy.GetParameterAsText(0)
 arcpy.env.workspace= folder_path
 
-#Masking file.
-Mask_file= arcpy.GetParameterAsText(1)
+#Clipping file.
+Clip_file= arcpy.GetParameterAsText(1)
 
 #For all the rasters in the file, perform an extract by mask.
-for rasters in arcpy.ListRasters():
-    arcpy.AddMessage(rasters)
-    #Out name is the Output File name. EBM stands for "Extract By Mask".
-    Out_Name= "EBM_" + rasters
-    outExtractByMask = ExtractByMask(rasters, Mask_file)
-    outExtractByMask.save(Out_Name)
+for shps in arcpy.ListFeatureClasses():
+    if shps != Clip_file:
+        arcpy.AddMessage(shps)
+        #Out name is the Output File name.
+        Out_Name= "Clip_" + shps
+        arcpy.Clip_analysis(shps, Clip_file, Out_Name)
